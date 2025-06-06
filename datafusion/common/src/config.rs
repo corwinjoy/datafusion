370a5f75c67d680c3f02a4108a95ccfd8c42371f
Adam Reeve <adam.reeve@gr-oss.io>
2025-06-06 16:56:15 +1200

    Downgrade parquet crate to work around regression

--- datafusion/common/src/config.rs
+++ datafusion/common/src/config.rs
@@ -194,6 +194,7 @@ macro_rules! config_namespace {
         }
     }
 }
+
 config_namespace! {
     /// Options related to catalog and directory scanning
     ///
@@ -2196,9 +2197,9 @@ impl From<ConfigFileDecryptionProperties> for FileDecryptionProperties {
         .with_column_keys(column_names, column_keys)
         .unwrap();
 
-        if !val.footer_signature_verification {
-            fep = fep.disable_footer_signature_verification();
-        }
+        //if !val.footer_signature_verification {
+        //    fep = fep.disable_footer_signature_verification();
+        //}
 
         if !val.aad_prefix_as_hex.is_empty() {
             let aad_prefix =
@@ -2235,7 +2236,7 @@ impl From<&FileDecryptionProperties> for ConfigFileDecryptionProperties {
             ),
             column_decryption_properties,
             aad_prefix_as_hex: hex::encode(aad_prefix),
-            footer_signature_verification: f.check_plaintext_footer_integrity(),
+            footer_signature_verification: true, //f.check_plaintext_footer_integrity(),
         }
     }
 }
